58e11a4897932edf204e27463b9aa6eeab2db3f7
Pranad Waghmare <pranadwaghmare@gmail.com>
2022-04-10 18:52:29 +0530

    Viewers added.

--- disney_plus_clone/src/components/Header/Header.js
+++ disney_plus_clone/src/components/Header/Header.js
@@ -50,6 +50,7 @@ const Nav = styled.nav`
   display: flex;
   align-items: center;
   padding: 0 36px;
+  overflow-x: hidden;
 `
 
 const Logo = styled.img`
@@ -90,7 +91,7 @@ const NavMenu = styled.div`
         bottom: -6px;
         opacity: 0;
         transform-origin: left center;
-        transition: all 250ms cubic-bezier(0.25,0.46,0.45,0.94) 0s;
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
         transform: scaleX(0);
       }
     }
